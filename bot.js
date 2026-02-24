e35102b97bc5e25bb4f257f407bc3f01104c2c6b
lauraexplora <74592807+lauraexplora@users.noreply.github.com>
2026-02-24 14:19:52 +0100

    word wrap

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const { Bot } = require("grammy");
 const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://mqtt.m5stack.com");
-require('dotenv').config();
+const wordwrap = require('wordwrapjs');
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
@@ -15,7 +17,7 @@ client.on("connect", () => {
 
 bot.on("message:text", (ctx) => {
     ctx.reply("Sending to printer: " + ctx.message.text);
-    client.publish(process.env.PRINTER_MAC, "TEXT,0,0:"+ ctx.message.text);
+    client.publish(process.env.PRINTER_MAC, "TEXT,0,0:"+ wordwrap.wrap(ctx.message.text, { width: 32 }));
 });
 
 bot.start();
