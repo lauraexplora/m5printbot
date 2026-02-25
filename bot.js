69ddfca639e5e56dad4aa68b4cde6306f4dd1465
lauraexplora <74592807+lauraexplora@users.noreply.github.com>
2026-02-25 11:37:18 +0100

    add qr codes for URLs

--- bot.js
+++ bot.js
@@ -1,23 +1,32 @@
-require('dotenv').config();
+import "dotenv/config.js";
 
-const { Bot } = require("grammy");
-const mqtt = require("mqtt");
+import { Bot } from "grammy";
+import mqtt from "mqtt";
+import wordwrap from "wordwrapjs";
+import getUrls from "get-urls";
+
 const client = mqtt.connect("mqtt://mqtt.m5stack.com");
-const wordwrap = require('wordwrapjs');
-
 const bot = new Bot(process.env.BOT_TOKEN);
 
 client.on("connect", () => {
   client.subscribe(process.env.PRINTER_MAC, (err) => {
     if (err) {
-        console.log(err);
+      console.log(err);
     }
   });
 });
 
 bot.on("message:text", (ctx) => {
-    ctx.reply("Sending to printer: " + ctx.message.text);
-    client.publish(process.env.PRINTER_MAC, "TEXT,0,0:"+ wordwrap.wrap(ctx.message.text, { width: 32 }));
+  const urls = getUrls(ctx.message.text);
+
+  ctx.reply("Sending to printer: " + ctx.message.text);
+  client.publish(
+    process.env.PRINTER_MAC,
+    "TEXT,0,0:" + wordwrap.wrap(ctx.message.text, { width: 32 }),
+  );
+  urls.forEach((url) => {
+    client.publish(process.env.PRINTER_MAC, "QR:" + url);
+  });
 });
 
 bot.start();
